c560c059df832737eb492be67619dce99ffd1c3f
Jan Hovancik <conta.srdr@gmail.com>
2018-04-29 20:48:39 +0200

    Creates accounts and dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'homes#show'
   resource :homes, only: [:show]
+  get '/dashboard', to: 'dashboard#show'
   resources :sessions, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
