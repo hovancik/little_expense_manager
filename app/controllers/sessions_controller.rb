5986f3dedf594a072bb83fa89b12240246c59863
Jan Hovancik <conta.srdr@gmail.com>
2018-05-03 20:24:41 +0200

    Edit main menu
    always show “Home”, add “Dashboard”

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,6 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: session_params[:email])
-    puts user
     if user && user.authenticate(session_params[:password])
       session[:auth_token] = user.auth_token
       render json: {
