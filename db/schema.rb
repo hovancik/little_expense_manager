7ae89545a1c61c24eca80ffca97de61912b9fda3
Jan Hovancik <conta.srdr@gmail.com>
2018-05-08 19:13:00 +0200

    Adds note to expense

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_01_161240) do
+ActiveRecord::Schema.define(version: 2018_05_08_114312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2018_05_01_161240) do
     t.decimal "amount", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "note"
     t.index ["account_id"], name: "index_expenses_on_account_id"
     t.index ["category_id"], name: "index_expenses_on_category_id"
     t.index ["payer_id"], name: "index_expenses_on_payer_id"
