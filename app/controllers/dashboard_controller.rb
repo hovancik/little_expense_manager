377fafd84bdeb9b26ba26c39d66b42948eaf5809
Jan Hovancik <conta.srdr@gmail.com>
2018-05-13 17:49:06 +0200

    Oder accounts in order by name

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,6 @@ class DashboardController < ApplicationController
   before_action :require_login
 
   def show
-    @accounts = current_user.accounts
+    @accounts = current_user.accounts.order(:name)
   end
 end
