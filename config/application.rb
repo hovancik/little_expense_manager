4146903b636098c1ebe628c2375b6244fb5e617f
Jan Hovancik <jan@hovancik.net>
2023-01-15 16:09:36 +0100

    Move to Heroku and update deps (#132)

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,9 @@ module LittleExpenseManager
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # https://bloggie.io/@kinopyo/heroku-free-dyno-with-cloudflare-free-ssl
+    require "cloudflare_proxy"
+    config.middleware.use CloudflareProxy
   end
 end
